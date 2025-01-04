5f9225fd0a5d3a76b189c53a98bbf9c4c04a5353
M.K.Raghul <mailtooraghul@gmail.com>
2025-01-04 22:52:04 +0530

    Add files via upload
    Star pattern

---
+++ Star.java
@@ -0,0 +1,23 @@
+package tryExample;
+
+import java.util.Scanner;
+
+public class Star {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enetr the Element:");
+		
+		int a[][]=new int[5][5];
+		for(int i=1;i<=a[i].length;i++)
+		{
+			for(int j=1;j<=a[j].length;j++)
+			{
+				System.out.println(a[i][j] +" *");
+			}
+		System.out.println();
+		}
+	}
+
+}
