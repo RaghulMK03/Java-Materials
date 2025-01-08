b74a3584136936bedcb41f91c3e66cc78ddecd3b
M.K.Raghul <mailtooraghul@gmail.com>
2025-01-08 23:54:03 +0530

    Add files via upload

---
+++ Looping.java
@@ -0,0 +1,24 @@
+package Iterative;
+
+public class Looping {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*//for loop 
+		int i;
+		for(i=1;i<=7;i++)
+		{
+			System.out.println("RAghul");
+		}*/
+		
+		//while loop
+		int i=1;
+		while(i<=5)
+		{
+			System.out.println("Raghul");
+			i++;
+		}
+
+	}
+
+}
