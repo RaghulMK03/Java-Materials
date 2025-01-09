1190426cff6b99914c3bda35e316d1f1e85c40e9
M.K.Raghul <mailtooraghul@gmail.com>
2025-01-09 23:33:28 +0530

    Add files via upload
    dsa

---
+++ intro/Main.java
@@ -0,0 +1,18 @@
+package intro;
+
+import java.util.Stack;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Stack<String> stack = new Stack<String>();
+		
+//		stack.push("raghul");
+		System.err.println(stack.empty());
+		
+		
+
+	}
+
+}
